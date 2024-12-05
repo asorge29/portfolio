7ffd957d1cb0aba169388cf3677bb990f8b8e9bc
Andy Sorge <124544413+asorge29@users.noreply.github.com>
2024-12-04 23:27:25 -0800

    New layout and theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,15 @@
-import Image from "next/image";
 import styles from "../styles/page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.header}>
-        <h1>Andy Sorge - Developer</h1>
-        <div className={styles.sectionLinks}>
-          <h2 className={styles.sectionLink}>About</h2>
-          <h2 className={styles.sectionLink}>Projects</h2>
-          <h2 className={styles.sectionLink}>Experience</h2>
-          <h2 className={styles.sectionLink}>Education</h2>
-          <h2 className={styles.sectionLink}>Contact</h2>
-        </div>
+      <div className={styles.greeting}>
+        <h1>&#47;&#47;Hi, I&apos;m Andy</h1>
+        <svg fill="var(--red)" height="100px" width="100px" xmlns="http://www.w3.org/2000/svg"
+             viewBox="0 0 512 512" enable-background="new 0 0 512 512" className={styles.downArrow}>
+<polygon points="289.7,341.3 289.7,0 204.3,0 204.3,341.3 33.7,170.7 33.7,298.7 247,512 460.3,298.7 460.3,170.7 "/>
+</svg>
       </div>
-
-      <div className={styles.imageContainer}>
-        <Image src="/andy.jpg" alt="Andy Sorge" fill className={styles.image} />
-      </div>
-      
     </main>
   )
 }
