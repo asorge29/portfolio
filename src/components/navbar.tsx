afbdd1dc22b2c3a1cbf3ea8aa57bfead8032332f
Andy Sorge <124544413+asorge29@users.noreply.github.com>
2024-12-06 09:14:37 -0800

    navbar glow by page

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,15 +1,20 @@
+'use client'
+
 import styles from "../styles/navbar.module.css"
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 export default function Navbar() {
 
+  const pathname = usePathname();
+
   return (
     <div className={styles.links}>
-        <Link href="/" className={styles.link}>Welcome</Link>
-        <Link href="/about" className={styles.link}>About</Link>
-        <Link href="/projects" className={styles.link}>Projects</Link>
-        <Link href="/experience" className={styles.link}>Experience</Link>
-        <Link href="/education" className={styles.link}>Education</Link>
+        <Link href="/" className={`${styles.link} ${pathname === '/' ? styles.current : ''}`}>Welcome</Link>
+        <Link href="/about" className={`${styles.link} ${pathname === '/about' ? styles.current : ''}`}>About</Link>
+        <Link href="/projects" className={`${styles.link} ${pathname === '/projects' ? styles.current : ''}`}>Projects</Link>
+        <Link href="/experience" className={`${styles.link} ${pathname === '/experience' ? styles.current : ''}`}>Experience</Link>
+        <Link href="/education" className={`${styles.link} ${pathname === '/education' ? styles.current : ''}`}>Education</Link>
       </div>
   )
 }
