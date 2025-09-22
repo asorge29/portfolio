5412b6d2fd8dc85796ee6e13d7df9290984b8e56
Andy Sorge <124544413+asorge29@users.noreply.github.com>
2025-09-21 19:30:48 -0700

    setup index page

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,33 @@
-import Image from "next/image";
+import {ArrowBigDownDash, ArrowBigRightDash, AtSign, Github, Linkedin} from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main className="relative">
+      <div className="h-screen w-screen flex flex-col items-center justify-center p-8">
+        <div className="bg-[url(/Andy11.jpg)] bg-cover bg-[position:18%_60%] bg-size-[250%] w-[30vh] h-[30vh] rounded-full"></div>
+        <h1 className="font-[Caprasimo] md:text-[160px] text-center text-4xl md:-m-2">
+          Hi. I&apos;m Andy.
+        </h1>
+        <p className="md:text-3xl text-center font-semibold">Student. Developer. Biker. Explorer. Brooklyn Nine-Nine Enthusiast.</p>
+        <div className="flex flex-row gap-4 w-min text-[4vh] mt-4">
+          <a className="hover:underline cursor-pointer flex flex-row items-center" target="_blank" href="https://github.com/asorge29"><Github className="h-[1.1em] w-[1.1em]" /></a>
+          <a className="hover:underline cursor-pointer flex flex-row items-center" target="_blank" href="https://linkedin.com/in/asorge29"><Linkedin className="h-[1.1em] w-[1.1em]" /></a>
+          <a className="hover:underl<br/>ine cursor-pointer flex flex-row items-center" target="_blank" href="mailto:hi@asorge29.dev"><AtSign className="h-[1.1em] w-[1.1em]" /></a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+      {/*<div className="absolute bottom-0 w-full flex items-center justify-center p-8">*/}
+      {/*  <Link href="/about" className="flex flex-col items-center text-3xl font-semibold">*/}
+      {/*    About*/}
+      {/*    <ArrowBigDownDash className="h-[1.1em] w-[1.1em]" />*/}
+      {/*  </Link>*/}
+      {/*</div>*/}
+      {/*<div className="absolute right-0 top-0 h-full flex items-center justify-center p-8">*/}
+      {/*  <Link href="/projects" className="flex flex-row items-center text-3xl font-semibold">*/}
+      {/*    Projects&nbsp;*/}
+      {/*    <ArrowBigRightDash className="h-[1.1em] w-[1.1em]" />*/}
+      {/*  </Link>*/}
+      {/*</div>*/}
+    </main>
   );
 }
