b7904ca7620c5fb36dee624d36e76545e862aa35
Andy Sorge <124544413+asorge29@users.noreply.github.com>
2025-09-21 22:09:37 -0700

    Page viewTransitions and about me page

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,34 @@
 import {ArrowBigDownDash, ArrowBigRightDash, AtSign, Github, Linkedin} from "lucide-react";
 import Link from "next/link";
+import {unstable_ViewTransition as ViewTransition} from "react";
 
 export default function Home() {
   return (
     <main className="relative">
       <div className="h-screen w-screen flex flex-col items-center justify-center p-8">
-        <div className="bg-[url(/Andy11.jpg)] bg-cover bg-[position:18%_60%] bg-size-[250%] w-[30vh] h-[30vh] rounded-full"></div>
-        <h1 className="font-[Caprasimo] md:text-[160px] text-center text-4xl md:-m-2">
-          Hi. I&apos;m Andy.
-        </h1>
+        <ViewTransition name="name">
+          <div className="bg-[url(/Andy11.jpg)] bg-cover bg-[position:18%_60%] bg-size-[250%] w-[30vh] h-[30vh] rounded-full"></div>
+          <h1 className="font-[Caprasimo] md:text-[160px] text-center text-4xl md:-m-2">
+            Hi. I&apos;m Andy.
+          </h1>
+        </ViewTransition>
         <p className="md:text-3xl text-center font-semibold">Student. Developer. Biker. Explorer. Brooklyn Nine-Nine Enthusiast.</p>
-        <div className="flex flex-row gap-4 w-min text-[4vh] mt-4">
-          <a className="hover:underline cursor-pointer flex flex-row items-center" target="_blank" href="https://github.com/asorge29"><Github className="h-[1.1em] w-[1.1em]" /></a>
-          <a className="hover:underline cursor-pointer flex flex-row items-center" target="_blank" href="https://linkedin.com/in/asorge29"><Linkedin className="h-[1.1em] w-[1.1em]" /></a>
-          <a className="hover:underl<br/>ine cursor-pointer flex flex-row items-center" target="_blank" href="mailto:hi@asorge29.dev"><AtSign className="h-[1.1em] w-[1.1em]" /></a>
-        </div>
+        <ViewTransition name="links">
+          <div className="flex flex-row gap-4 w-min text-[4vh] mt-4">
+            <a className="hover:underline cursor-pointer flex flex-row items-center" target="_blank" href="https://github.com/asorge29"><Github className="h-[1.1em] w-[1.1em]" /></a>
+            <a className="hover:underline cursor-pointer flex flex-row items-center" target="_blank" href="https://linkedin.com/in/asorge29"><Linkedin className="h-[1.1em] w-[1.1em]" /></a>
+            <a className="hover:underl<br/>ine cursor-pointer flex flex-row items-center" target="_blank" href="mailto:hi@asorge29.dev"><AtSign className="h-[1.1em] w-[1.1em]" /></a>
+          </div>
+        </ViewTransition>
       </div>
-      {/*<div className="absolute bottom-0 w-full flex items-center justify-center p-8">*/}
-      {/*  <Link href="/about" className="flex flex-col items-center text-3xl font-semibold">*/}
-      {/*    About*/}
-      {/*    <ArrowBigDownDash className="h-[1.1em] w-[1.1em]" />*/}
-      {/*  </Link>*/}
-      {/*</div>*/}
+      <div className="absolute bottom-0 w-full flex items-center justify-center p-8">
+        <ViewTransition name="about">
+          <Link href="/about" className="flex flex-col items-center text-3xl font-semibold">
+            About Me
+            <ArrowBigDownDash className="h-[1.1em] w-[1.1em]" />
+          </Link>
+        </ViewTransition>
+      </div>
       {/*<div className="absolute right-0 top-0 h-full flex items-center justify-center p-8">*/}
       {/*  <Link href="/projects" className="flex flex-row items-center text-3xl font-semibold">*/}
       {/*    Projects&nbsp;*/}
